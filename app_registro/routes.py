1ba73c35bd5ecd03ce41bbea2b760ac2ab72ad95
unknown <dulce.1201@gmail.com>
2023-01-06 23:42:33 +0100

    uso de csv, e import de datetime para comaración de fechas incluidas en el form

--- app_registro/routes.py
+++ app_registro/routes.py
@@ -1,30 +1,47 @@
 from app_registro import app
-from flask import render_template
+from flask import render_template, request, redirect
 import csv
+from datetime import datetime
 
 @app.route("/")
 def index():
-	#Se remplaza el diccionario datos por archivos csv
-	fichero = open("data/movimientos.txt", "r") #Llama al archivo
-	csvReader = csv.reader(fichero, delimiter=",", quotechar="'") #accede a cada registro del archivo y le da nuevo formato
+    #Se remplaza el diccionario datos por archivos csv
+    fichero = open("data/movimientos.csv", "r") #Llama al archivo en modo lectura
+    lectura = csv.reader(fichero, delimiter=",", quotechar="'") #accede a cada registro del archivo y le da nuevo formato csv
 
-	datos = []
+    datos = []
 
-	for item in csvReader:
-		datos.append(item) #recorre el obj csvReader y carga cada registro en el array vacío
+    for item in lectura:
+        datos.append(item) #recorre el obj csvReader y carga cada registro en el array vacío
 
-	return render_template("index.html", pageTitle="Listas", lista=datos)
+    return render_template("index.html", pageTitle="Listas", lista=datos)
 
 @app.route("/new",methods=["POST", "GET"])
 def create():
-	return render_template("new.html", pageTitle="Alta", typeAction = "Alta", buttonAction = "Guardar")
+    if request.method == "GET":
+        return render_template("new.html", pageTitle="Alta", typeAction = "Alta", buttonAction = "Guardar")
+    else:
+        fichero = open("data/movimientos.csv", "a", newline="") #Llamo al archivo preparado para crear un nuevo registro
+        escritura = csv.writer(fichero, delimiter=",", quotechar="'") #uso el método writer y le doy formato csv a los registros
+        
+        today = datetime.now()
+        fecha_ingresada = datetime.strptime(request.form['date'], "%Y-%m-%d").date()
+        if fecha_ingresada <= today.date():
+            print("fecha correcta",today.date())
+            escritura.writerow([request.form['date'], request.form['concept'], request.form['quantity']]) #registro los datos recibidos desde el request.form y agrego con el metodo writerow
+        else:
+            print("fecha incorrecta")
+
+        fichero.close()
+
+    return redirect("/")
 
 @app.route("/update")
 def edit():
-	return render_template("update.html", pageTitle="Modificar", typeAction = "Modificación", buttonAction = "Editar")
+    return render_template("update.html", pageTitle="Modificar", typeAction = "Modificación", buttonAction = "Editar")
 
 @app.route("/delete")
 def remove():
-	return render_template("delete.html", pageTitle="Eliminar")
+    return render_template("delete.html", pageTitle="Eliminar")
 
 
