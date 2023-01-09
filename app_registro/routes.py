0623284d21bcd66ed4fd5bb6b22c2349f989ad1c
unknown <dulce.1201@gmail.com>
2023-01-09 21:41:07 +0100

    Completada función borrar

--- app_registro/routes.py
+++ app_registro/routes.py
@@ -2,11 +2,13 @@ from app_registro import app
 from flask import render_template, request, redirect
 import csv
 from datetime import datetime
+from config import *
+import os #modilo para renombrar y eliminar archivos
 
 @app.route("/")
 def index():
     #Se remplaza el diccionario datos por archivos csv
-    fichero = open("data/movimientos.csv", "r") 
+    fichero = open(MOVIMIENTOS_FILE, "r") 
     #Llama al archivo en modo lectura
     lectura = csv.reader(fichero, delimiter=",", quotechar="'") 
     #preparo cada registro del archivo y le da nuevo formato csv
@@ -30,13 +32,13 @@ def create():
         if error:
             return render_template("new.html", pageTitle="Alta", typeAction = "Alta", buttonAction = "Guardar", msjError = error, dataForm = request.form)
         else:
-            mi_fichero = open("data/movimientos.csv", "a", newline="") 
+            mi_fichero = open(MOVIMIENTOS_FILE, "a", newline="") 
             #Llamo al archivo preparado para crear un nuevo registro
             escritura = csv.writer(mi_fichero, delimiter=",", quotechar="'") 
             #preparo el formato csv a que tendrá un nuevo registro
 
             #---->Para identificar y crear id(#). Consulto el archivo donde se guardan los últimos registros
-            fichero_r = open("data/last_id.csv", "r") 
+            fichero_r = open(LAST_ID_FILE, "r") 
             registro = fichero_r.read()
             if registro == "":
                 new_id = 1
@@ -44,7 +46,7 @@ def create():
                 new_id = int(registro)+1
             fichero_r.close()
 
-            fichero_w = open("data/last_id.csv", "w")
+            fichero_w = open(LAST_ID_FILE, "w")
             fichero_w.write(str(new_id))
             fichero_w.close
 
@@ -57,13 +59,50 @@ def create():
 
 @app.route("/update/<int:id>")
 def edit(id):
-    return f"{id} a editar"
-    #return render_template("update.html", pageTitle="Modificar", typeAction = "Modificación", buttonAction = "Editar")
+    #return f"{id} a editar"
+    return render_template("update.html", pageTitle="Modificar", typeAction = "Modificación", buttonAction = "Editar", dataForm = {})
 
-@app.route("/delete/<int:id>")
+@app.route("/delete/<int:id>",methods= ["GET", "POST"])
 def remove(id):
-    return f"{id} a borrar"
-    #return render_template("delete.html", pageTitle="Eliminar")
+    #return f"{id} a borrar"
+     
+    if request.method == "GET":
+        mi_fichero = open(MOVIMIENTOS_FILE, "r")
+        lectura = csv.reader(mi_fichero, delimiter=",", quotechar="'") 
+        registro_buscado = []
+
+        for registro in lectura:
+            if registro[0] == str(id):
+                registro_buscado = registro
+        mi_fichero.close()
+        #?????
+
+        if len(registro_buscado) > 0:
+            return render_template("delete.html", pageTitle="Eliminar", registro= registro_buscado)
+        else: 
+            return redirect("/")
+
+    else: 
+        fichero_old = open(MOVIMIENTOS_FILE, "r")
+        #Llamo al archivo de registros
+        fichero_new = open(MOVIMIENTOS_FILE_NEW, "w", newline="")
+        #Llamo a un archivo nuevo (recién creado)
+
+        lectura = csv.reader(fichero_old, delimiter=",", quotechar="'") 
+        escritura = csv.writer(fichero_new, delimiter=",", quotechar="'")
+
+        for registro in lectura:
+            if registro[0] != str(id):
+                escritura.writerow(registro)
+                #????
+
+        fichero_old.close()
+        fichero_new.close()
+
+        os.remove(MOVIMIENTOS_FILE)
+        os.rename(MOVIMIENTOS_FILE_NEW, MOVIMIENTOS_FILE)
+
+        return redirect("/")
 
 
 def validateForm(requestForm):
