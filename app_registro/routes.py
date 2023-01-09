e6d3d2ba050b088c7f1c611332e4e2c9c02e1654
unknown <dulce.1201@gmail.com>
2023-01-09 18:29:24 +0100

    actualizar función de nuevo registro. Creación de nuevo archivo csv para hacer referencia al id del último registro hecho

--- app_registro/routes.py
+++ app_registro/routes.py
@@ -9,14 +9,15 @@ def index():
     fichero = open("data/movimientos.csv", "r") 
     #Llama al archivo en modo lectura
     lectura = csv.reader(fichero, delimiter=",", quotechar="'") 
-    #accede a cada registro del archivo y le da nuevo formato csv
+    #preparo cada registro del archivo y le da nuevo formato csv
 
     datos = []
 
     for item in lectura:
         datos.append(item) 
-        #Recorre el obj csvReader y carga cada registro en el array vacío
-
+        #Recorre el obj csvReader y carga cada registro en el array vacío pra nostrarlos en pantalla
+    
+    fichero.close()
     return render_template("index.html", pageTitle="Listas", lista=datos)
 
 @app.route("/new",methods=["POST", "GET"])
@@ -24,43 +25,45 @@ def create():
     if request.method == "GET":
         return render_template("new.html", pageTitle="Alta", typeAction = "Alta", buttonAction = "Guardar", dataForm = {})
     else:
-        fichero = open("data/movimientos.csv", "a", newline="") 
-        #Llamo al archivo preparado para crear un nuevo registro
-        escritura = csv.writer(fichero, delimiter=",", quotechar="'") 
-        #Uso el método writer y le doy formato csv a los registros
-        
         error = validateForm(request.form)
 
         if error:
             return render_template("new.html", pageTitle="Alta", typeAction = "Alta", buttonAction = "Guardar", msjError = error, dataForm = request.form)
         else:
-            escritura.writerow([request.form['date'], request.form['concept'], request.form['quantity']])
+            mi_fichero = open("data/movimientos.csv", "a", newline="") 
+            #Llamo al archivo preparado para crear un nuevo registro
+            escritura = csv.writer(mi_fichero, delimiter=",", quotechar="'") 
+            #preparo el formato csv a que tendrá un nuevo registro
 
-        '''
-        OTRA FORMA DE HACERLO si importo date en lugar de datetime (sin función de validación de errores)
-        hoy = datetime.now()
-        fecha_ingresada = datetime.strptime(request.form['date'], "%Y-%m-%d").date() 
-        #doy formato a la fecha ingresada
-        if fecha_ingresada <= hoy.date():
-            print("fecha correcta",hoy.date())
-            escritura.writerow([request.form['date'], request.form['concept'], request.form['quantity']]) 
-            #registro los datos recibidos desde el request.form y agrego con el metodo writerow
-        else:
-            print("fecha incorrecta")
-            return render_template("new.html", pageTitle="Alta", typeAction = "Alta", buttonAction = "Guardar")
-        '''
+            #---->Para identificar y crear id(#). Consulto el archivo donde se guardan los últimos registros
+            fichero_r = open("data/last_id.csv", "r") 
+            registro = fichero_r.read()
+            if registro == "":
+                new_id = 1
+            else:
+                new_id = int(registro)+1
+            fichero_r.close()
 
-        fichero.close()
+            fichero_w = open("data/last_id.csv", "w")
+            fichero_w.write(str(new_id))
+            fichero_w.close
+
+            escritura.writerow([new_id, request.form['date'], request.form['concept'], request.form['quantity']])
+            #Uso el metodo writerow de write para escribir el registro
+
+        mi_fichero.close()
 
     return redirect("/")
 
-@app.route("/update")
-def edit():
-    return render_template("update.html", pageTitle="Modificar", typeAction = "Modificación", buttonAction = "Editar")
+@app.route("/update/<int:id>")
+def edit(id):
+    return f"{id} a editar"
+    #return render_template("update.html", pageTitle="Modificar", typeAction = "Modificación", buttonAction = "Editar")
 
-@app.route("/delete")
-def remove():
-    return render_template("delete.html", pageTitle="Eliminar")
+@app.route("/delete/<int:id>")
+def remove(id):
+    return f"{id} a borrar"
+    #return render_template("delete.html", pageTitle="Eliminar")
 
 
 def validateForm(requestForm):
@@ -74,3 +77,17 @@ def validateForm(requestForm):
     if requestForm['quantity'] == "" or float(requestForm['quantity']) == 0.0:
         errores.append('Cantidad vacía o cero: Introduce una cantidad positiva o negativa')
     return errores
+
+
+'''
+OTRA FORMA DE HACERLO si importo date en lugar de datetime (sin función de validación de errores)
+hoy = datetime.now()
+fecha_ingresada = datetime.strptime(request.form['date'], "%Y-%m-%d").date() 
+#doy formato a la fecha ingresada
+if fecha_ingresada <= hoy.date():
+    print("fecha correcta",hoy.date())
+     escritura.writerow([request.form['date'], request.form['concept'], request.form['quantity']]) 
+    #registro los datos recibidos desde el request.form y agrego con el metodo writerow
+else:
+    return render_template("new.html", pageTitle="Alta", typeAction = "Alta", buttonAction = "Guardar")
+'''
